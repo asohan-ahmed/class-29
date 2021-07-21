70b86a7421e86106bc6c7b2939c536394bd0f0b3
Sohan Ahmed <82075290+asohaahmed@users.noreply.github.com>
2021-07-21 12:08:22 +0600

    please follow me

---
+++ index.js
@@ -0,0 +1,17 @@
+// hi evryone what's up im learning javascript that's why im try to this type problem solveing
+var sohan = Math.floor(Math.random()*100) +1;
+document.write(sohan)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
